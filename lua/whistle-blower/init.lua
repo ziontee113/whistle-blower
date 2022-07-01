449876f4565346f54bdb45102b6c55bc976e3b22
ziontee113 <ziontee113@gmail.com>
2022-07-02 06:47:49 +0700

    move test_extmark to separate file

--- lua/whistle-blower/init.lua
+++ lua/whistle-blower/init.lua
@@ -1,18 +1,10 @@
 local M = {}
 local api = vim.api
 
-local ns = api.nvim_create_namespace("whistle-blower")
-
-local test_extmark
-
-api.nvim_buf_clear_namespace(0, ns, 0, -1)
+local test_extmark_fn = require("whistle-blower.utils.extmarks").test_extmark
 
 vim.keymap.set("n", "<F24><F24>h", function()
-	local curLine = api.nvim_win_get_cursor(0)[1] - 1
-	test_extmark = api.nvim_buf_set_extmark(0, ns, curLine, -1, {
-		virt_text = { { "|||", "CmpItemKindClass" } },
-		-- virt_text_win_col = 40,
-	})
+	test_extmark_fn()
 end, { noremap = true, silent = true })
 
 return M
