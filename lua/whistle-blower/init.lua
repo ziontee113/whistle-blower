5ab5b8b2a24f6b3b7e1beb09c198f338a830a2d7
ziontee113 <ziontee113@gmail.com>
2022-07-04 14:26:48 +0700

    add viewport.lua

--- lua/whistle-blower/init.lua
+++ lua/whistle-blower/init.lua
@@ -1,10 +1,13 @@
 local M = {}
 local api = vim.api
 
-local test_extmark_fn = require("whistle-blower.utils.extmarks").test_extmark
+-- required functions
+local utils_extmarks = require("whistle-blower.utils.extmarks")
+local utils_viewport = require("whistle-blower.utils.viewport")
 
+-- keymaps for testing
 vim.keymap.set("n", "<F24><F24>h", function()
-	test_extmark_fn()
+	utils_extmarks.test_extmark()
 end, { noremap = true, silent = true })
 
 return M
